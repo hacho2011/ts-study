826254dd4212844d9164ad0e8f917df2ee181492
hacho2011 <hacho_2011@naver.com>
2021-03-26 22:59:13 +0900

    함수 타입 이용

--- 1-type/1-2-function.ts
+++ 1-type/1-2-function.ts
@@ -1,17 +1,49 @@
 {
-    // javascript x
-    function jsAdd(num1, num2){
-        return num1 + num2; 
+    // // javascript x
+    // function jsAdd(num1, num2){
+    //     return num1 + num2; 
+    // }
+
+    // function add(num1 : number, num2: number):number{
+    //     return num1 + num2; 
+    // }
+
+    // function jsFecthNum(id) {
+
+    //     return new Promise((resolve, reject) => {
+    //         resolve(100);
+    //     })
+    // }
+
+    // function fecthNum(id:string):Promise<number> {
+
+    //     return new Promise((resolve, reject) => {
+    //         resolve(100);
+    //     })
+    // }
+
+    //javascript =>  typescript 
+
+    function printName(firstName:string, lastName?: string){
+        console.log(firstName)
+        console.log(lastName)
     }
 
-    function add(num1 : number, num2: number):number{
-        return num1 + num2; 
+    printName('Steve', 'Jobs');
+    printName('Steve');
+    printName('Steve', 'Jobs');
+
+    // default parameter 
+    function printMessage(message: string = 'default message'){
+        console.log(message);
     }
 
-    function jsFecthNum(id) {
+    printMessage();
 
-        return new Promise((resolve, reject) => {
-            resolve(100);
-        })
+    // rest parameter 
+    function addNumbers(...numbers: number[]):number {
+        return numbers.reduce((a,b)=> a+b);
     }
+
+    console.log(addNumbers(1,2,3,4,5));
 }
