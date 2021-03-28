8bca4635762495e97f5b8efde1ca8271a8073a8c
hacho2011 <hacho_2011@naver.com>
2021-03-28 22:52:20 +0900

    union

---
+++ 1-type/1-5-union.ts
@@ -0,0 +1,43 @@
+{
+    /**
+     * Union
+     */
+
+    type Direction = 'left' | 'right' | 'up' | 'down';
+    function move(direction:Direction){
+        console.log(direction)
+    }
+    move('down');
+
+    type TileSize = 8 | 12| 32; 
+    const tile: TileSize = 12;
+
+    //function: login => success, fail; 
+
+    type SuccessState= {
+        response: {
+            body: string; 
+        }
+    }
+    type FailState = {
+        reason: string; 
+    }
+
+    type LoginState = SuccessState | FailState;
+
+    // function login(id:string, password: string): Promise<LoginState> {
+    function login(): LoginState {
+        return  {
+            response: {
+                body: 'logged in!'
+            }
+        }
+    }
+
+    // function printLoginState(state: LoginState){
+    //     if(true){
+    //         return state.response; 
+    //     }
+    // }
+
+}
