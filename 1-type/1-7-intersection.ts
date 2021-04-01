1500e52c474bab9f91b1822777cdc587ebf042ae
hacho2011 <hacho_2011@naver.com>
2021-04-01 23:49:23 +0900

    intersection

---
+++ 1-type/1-7-intersection.ts
@@ -0,0 +1,26 @@
+{
+    /**
+     * IntersectionTypes
+     */
+
+    type Student = {
+        name: string; 
+        score: number; 
+    }
+
+    type Worker= {
+        employeeId: number; 
+        work: () => void;
+    }
+
+    function internWork(person: Student & Worker) {
+        console.log(person.name, person.employeeId, person.work());
+    }
+
+    internWork( {
+        name: 'ellie', 
+        score: 1,
+        employeeId : 1,
+        work: ()=> {}
+    })
+}
